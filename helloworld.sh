1bcd59509d4d5aebb335607933d6c8e604bf7c5b
dominiquebruso <nikki.f.bruso@gmail.com>
2023-10-24 15:54:42 -0700

    helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 echo "Hello World"
-# Script Name:                  Hello World
-# Author:                       Dominique Bruso
-# Date of latest revision:      10/24/2023
-# Purpose:                      To Say Hello
 
 my_variable="I am working perfectly!" 
 
 echo $my_variable
 
+# Script Name:                  Hello World
+# Author:                       Dominique Bruso
+# Date of latest revision:      10/24/2023
+# Purpose:                      To Say Hello
+
 # Declaration of variables: 1
 
 # Declaration of functions
 
-
 # Main
-
-
 
 # End
