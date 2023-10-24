89d34d08abafdf34439d26447882c0ced60f9af5
dominiquebruso <nikki.f.bruso@gmail.com>
2023-10-24 15:05:50 -0700

    helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Hello World"
+# Script Name:                  Hello World
+# Author:                       Dominique Bruso
+# Date of latest revision:      10/24/2023
+# Purpose:                      To Say Hello
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
