38570e492f8919c1ecc6a63ddba815f63bf05ca6
dominiquebruso <nikki.f.bruso@gmail.com>
2023-10-25 16:11:54 -0700

    function practice

---
+++ function.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script Name:                  Fuction test
+# Author:                       Dominique Bruso
+# Date of latest revision:      10/25/2023
+# Purpose:                      To practice making functions
+# Execution: bash function.sh or ./function.sh
+# Declaration of variables
+
+# Define a function 
+print_login_history() {
+  echo "This is the login history:"
+  last
+}
+
+# Call the function to print login history
+print_login_history
+print_login_history
+print_login_history
+
+# Main
+
+
+
+# End
