c93da1b311de9e4587773ee85c12d0daf8474909
dominiquebruso <nikki.f.bruso@gmail.com>
2023-10-26 16:49:36 -0700

    ops challenge 4

---
+++ arrays.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script Name:                  Arrays Pracitce
+# Author:                       Dominique Bruso
+# Date of latest revision:      10/2265/2023
+# Purpose:                      To practice making arrays
+# Execution:                    bash arrays.sh or ./arrays.sh
+
+# # Create the four directories
+mkdir dir1 dir2 dir3 dir4
+
+directories=("dir1" "dir2" "dir3" "dir4")
+
+# Create the .txt files in each directory
+touch dir1/dir1.txt
+touch dir2/dir2.txt
+touch dir3/dir3.txt
+touch dir4/dir4.txt
+
+
+# Main
+
+
+
+# End
