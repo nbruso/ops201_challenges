736f7e46c72b6d940859051ccc34ea779269635d
Dominique Bruso <dominiquebruso@labcomputer>
2024-02-12 14:54:47 -0700

    26

--- ops_401/challenge_26.py
+++ ops_401/challenge_26.py
@@ -11,18 +11,40 @@
 import logging
 import os
 
+# Import necessary libraries
+import paramiko  # For SSH connections
+import sys       # For system operations like exiting the script
+from zipfile import ZipFile  # For working with ZIP files
+
+# Create an SSH client object for SSH connections
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Automatically accept unknown host keys
+
+# Function to attempt SSH connection
+def ssh_connect(host, username, password):
+    try:
+        ssh.connect(host, port=22, username=username, password=password)  # Try to connect using the provided credentials
+        ssh.close()  # Close the connection after successful login
+        return True  # Return True if login is successful
+    except paramiko.AuthenticationException:
+        return False  # Return False if authentication fails
+    except paramiko.SSHException:
+        ssh.close()  # Ensure connection is closed on exception
+        return False  # Return False if other SSH exceptions occur
+
 # Create the log object
 log = logging.getLogger("my_logger")
 
 # Configure my logging object
-logging.basicConfig()
+logging.basicConfig(logging.basicConfig(filename='sssh_attempts.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s'))
 
-log.info("Hello, World")
-log.warning("THIS IS A WARNING")
+log.critical("CRITICAL ALERT")
+log.warning("WARNING:SSH ATTEMPT")
 
 #Define a function
-def do_something():
-log.debug("Doing something!")
+def ssh_alerts():
+log.critical("CRITICAL ALERT")
+log.warning("WARNING:SSH ATTEMPT")
 
 #Call our function
-do_something()
+ssh_alerts()
